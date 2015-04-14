28814d5393d508c1ead3a16c13b3b59ab532dd2f
tunnckoCore <mameto_100@mail.bg>
2015-04-14 04:22:48 +0300

    replace `micromatch` with `is-match`

--- index.js
+++ index.js
@@ -7,33 +7,14 @@
 
 'use strict';
 
-var is = require('is-kindof');
 var through2 = require('through2');
-var micromatch = require('micromatch');
+var matcher = require('is-match');
 
-module.exports = function gulpMicromatch(patterns, opts) {
-  if (!is.array(patterns) && !is.string(patterns) && !is.function(patterns) && !is.regexp(patterns)) {
-    throw new TypeError('gulp-micromatch expects array, string, function or regexp');
-  }
-
-  var isMatch = null;
-
-  if (is.array(patterns)) {
-    isMatch = function(fp) {
-      return micromatch(fp, patterns, opts).length;
-    }
-  }
-  if (is.string(patterns)) {
-    isMatch = function(fp) {
-      return micromatch.isMatch(fp, patterns, opts);
-    }
-  }
-  if (is.function(patterns) || is.regexp(patterns)) {
-    isMatch = micromatch.matcher(patterns, opts)
-  }
+module.exports = function gulpMicromatch(patterns, options) {
+  var isMatch = matcher(patterns, options);
 
   return through2.obj(function(file, enc, next) {
-    if (isMatch && isMatch(file.path)) {
+    if (isMatch(file.path)) {
       this.push(file);
     }
     next();
